47d81284e33d8eec8a8e62656852ec513f2cd5b8
Federico Garcia Criado <federogc@picasso.scbi.uma.es>
2021-09-29 21:02:01 +0200

    making the first daemon

--- daemon.sh
+++ daemon.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env bash
+
+
+$exec_mode=$1
+
+if [ "$exec_mode" == "download" ] ; then
+    #STAGE 1 DOWNLOADING REFERENCE 
+elif [ "$exec_mode" == "autoflow" ] ; then
+	#STAGE 2 AUTOFLOW EXECUTION
+	AutoFlow -w autoflow_template.af -V ontologia,kenertype,red -O
+elif [ "$exec_mode" == "report" ] ; then 
+	#STAGE 3 GENERATE REPORT fROM RESULTS
+fi
+
+
