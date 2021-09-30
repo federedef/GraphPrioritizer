e8d3a8b3b31d7fa9e4bd6e777db33453f2a12d01
Federico Garcia Criado <federogc@picasso.scbi.uma.es>
2021-09-30 23:39:23 +0200

    First steps with the autoflow template and daemon

--- daemon.sh
+++ daemon.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-
-$exec_mode=$1
+exec_mode=$1
+export annotations_files_folder = $SCRATCH/executions/backupgenes
 
 if [ "$exec_mode" == "download" ] ; then
     #STAGE 1 DOWNLOADING REFERENCE 
+
 elif [ "$exec_mode" == "autoflow" ] ; then
 	#STAGE 2 AUTOFLOW EXECUTION
-	AutoFlow -w autoflow_template.af -V ontologia,kenertype,red -O
+	AutoFlow -w autoflow_template.af -V '$ont='hp.obo',$net='gene_phen_filtered.paco',$kernel='ct'' -O annotations_files_folder
 elif [ "$exec_mode" == "report" ] ; then 
 	#STAGE 3 GENERATE REPORT fROM RESULTS
 fi
-
 
