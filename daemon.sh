a4a9638badba3f5c6bf2d440b987f381cce97083
Federico Garcia Criado <federogc@picasso.scbi.uma.es>
2021-10-06 12:04:13 +0200

    Preparing the code for semtools use and adding customize variables

--- daemon.sh
+++ daemon.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env bash
-. ~soft_bio_267/initializes/init_autoflow
-exec_mode=$1
+. ~soft_bio_267/initializes/init_autoflow 
+exec_mode=$1 # Modificar como Pepe lo tiene en su daemon aux_sh/trim_and_map.sh
 export annotations_files_folder=$SCRATCH/executions/backupgenes
 export results_files=/mnt/home/users/bio_267_uma/federogc/projects/backupgenes/report
+add_opt=$2 # Un string que damos como segundo argumento.
 
-if [ "$exec_mode" == "download" ] ; then
-    #STAGE 1 DOWNLOADING REFERENCE 
+#Custom variables.
+ont=hp.obo
+net=gene_phen_filtered.paco;loquesea.paco;...
+kernel=ct
+input_path=././
+
+autoflow_vars=`echo " 
+\\$ont=$ont,
+\\$nets=$net,
+\\$kernel=$kernel,
+\\$input_path=$input_path
+" | tr -d [:space:]`
+
+ if [ "$exec_mode" == "download" ] ; then
+     #STAGE 1 DOWNLOADING REFERENCE 
 
 elif [ "$exec_mode" == "autoflow" ] ; then
 	#STAGE 2 AUTOFLOW EXECUTION
-	AutoFlow -w autoflow_template.af -V '$ont='hp.obo',$net='gene_phen_filtered.paco',$kernel='ct'' -O annotations_files_folder
+	AutoFlow -w autoflow_template.af -V $autoflow_vars -O annotations_files_folder $add_opt # Confirmar con Pepe
 elif [ "$exec_mode" == "report" ] ; then 
 	#STAGE 3 GENERATE REPORT fROM RESULTS
 fi
