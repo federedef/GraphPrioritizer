72fbf3ad14414c09efe1908455a62d9a89e38ef2
Federico Garcia Criado <federogc@picasso.scbi.uma.es>
2021-10-09 16:20:40 +0200

    trying to make the iterative parte of calc kernel

--- daemon.sh
+++ daemon.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 . ~soft_bio_267/initializes/init_autoflow 
 exec_mode=$1 # Modificar como Pepe lo tiene en su daemon aux_sh/trim_and_map.sh
+add_opt=$2 # Un string que damos como segundo argumento.
 export annotations_files_folder=$SCRATCH/executions/backupgenes
 export results_files=/mnt/home/users/bio_267_uma/federogc/projects/backupgenes/report
-add_opt=$2 # Un string que damos como segundo argumento.
+
 
 #Custom variables.
 ont=hp.obo
-net=gene_phen_filtered.paco;loquesea.paco;...
+net=small_pro #;loquesea.paco;...
 kernel=ct
-input_path=././
+input_path=`pwd`
 
 autoflow_vars=`echo " 
 \\$ont=$ont,
@@ -18,14 +19,17 @@ autoflow_vars=`echo "
 \\$input_path=$input_path
 " | tr -d [:space:]`
 
- if [ "$exec_mode" == "download" ] ; then
-     #STAGE 1 DOWNLOADING REFERENCE 
+if [ "$exec_mode" == "download" ] ; then
+  #STAGE 1 DOWNLOADING REFERENCE
+  pwd
 
 elif [ "$exec_mode" == "autoflow" ] ; then
-	#STAGE 2 AUTOFLOW EXECUTION
-	AutoFlow -w autoflow_template.af -V $autoflow_vars -O annotations_files_folder $add_opt # Confirmar con Pepe
+  #STAGE 2 AUTOFLOW EXECUTION
+  AutoFlow -w autoflow_template.af -V $autoflow_vars -o $annotations_files_folder/exec # $add_opt Confirmar con Pepe
+
 elif [ "$exec_mode" == "report" ] ; then 
-	#STAGE 3 GENERATE REPORT fROM RESULTS
+  #STAGE 3 GENERATE REPORT fROM RESULTS
+  pwd
 fi
 
 
