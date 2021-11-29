83cfae570521beb7c225f89b13af4e52ba48e062
Federico Garcia Criado <federogc@picasso.scbi.uma.es>
2021-11-29 15:08:28 +0100

    Improving the report section.

--- daemon.sh
+++ daemon.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 . ~soft_bio_267/initializes/init_autoflow 
-exec_mode=$1 # Modificar como Pepe lo tiene en su daemon aux_sh/trim_and_map.sh
-add_opt=$2 # Un string que damos como segundo argumento.
+
+exec_mode=$1 
+add_opt=$2 # flags to autoflow
 input_path=`pwd`
 export PATH=$input_path/aux_scripts:$PATH 
 
 output_folder=$SCRATCH/executions/backupgenes
 autoflow_output=$output_folder/exec
 results_files=$output_folder/report
-# Añadir el export para el path.
+
 
 
 #Custom variables.
-#ont=hp.obo
 net="small_pro;small_pro_two" #;loquesea.paco;... gen_phen_mini; small_pro
 kernel="ct;rf"
 integration_types="mean;" #...;integration_mean_by_presence;...
 net2ont=$input_path'/net2ont' 
-gens_seed=$input_path'/gens_seed' #New
+gens_seed=$input_path'/gens_seed' # What are the knocked genes?
 
 autoflow_vars=`echo " 
 \\$nets=$net,
@@ -40,11 +40,16 @@ elif [ "$exec_mode" == "check" ] ; then
 elif [ "$exec_mode" == "report" ] ; then 
   #STAGE 3 GENERATE REPORT fROM RESULTS
   source ~soft_bio_267/initializes/init_ruby
-  mkdir $results_files
-  echo $autoflow_output/uncomb_kernel_metrics
-  create_metric_table.rb $autoflow_output/uncomb_kernel_metrics sample,net,kernel $results_files/parsed_uncomb_kmetrics
-  create_metric_table.rb $autoflow_output/comb_kernel_metrics sample,integration,kernel $results_files/parsed_comb_kmetrics
-  report_html -t report.erb -d $results_files/parsed_uncomb_kmetrics,$results_files/parsed_comb_kmetrics -o report_metrics
+
+  if [ ! -s $results_files ] ; then
+    mkdir $results_files
+  fi
+
+  #echo $autoflow_output/uncomb_kernel_metrics
+  create_metric_table.rb $autoflow_output/similarity_metrics Net $results_files/parsed_similarity_metrics
+  create_metric_table.rb $autoflow_output/uncomb_kernel_metrics Sample,Net,Kernel $results_files/parsed_uncomb_kmetrics
+  create_metric_table.rb $autoflow_output/comb_kernel_metrics Sample,Integration,Kernel $results_files/parsed_comb_kmetrics
+  report_html -t report.erb -d $results_files/parsed_uncomb_kmetrics,$results_files/parsed_comb_kmetrics,$results_files/parsed_similarity_metrics -o report_metrics
 fi
 
 
