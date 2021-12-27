55244b5605c33a3d32d96fc0686a89164614801c
Federico Garcia Criado <federogc@picasso.scbi.uma.es>
2021-12-27 19:32:48 +0100

    New script for density filter in similarity matrixes

---
+++ try_scripts/density_filter.R
@@ -0,0 +1,30 @@
+#!/usr/bin/env Rscript
+library(RcppCNPy)
+library(disparityfilter) #TODO: preguntar por la instalacion.
+library(igraph)
+library(optparse)
+
+option_list <- list(
+  make_option(c("-i", "--input_matrix"), type="character",
+              help="insert name of a npy file"),
+  make_option(c("-o", "--output_path"), default=".", type="character", default="output",
+              help="Output matrix path")
+)
+
+opt <- parse_args(OptionParser(option_list=option_list))
+
+# Load npy adjacency matrix and create graph object.
+sem_m <- npyLoad(opt$input_matrix)
+sem_g <- graph_from_adjacency_matrix(sem_m, mode="undirected",diag=TRUE,weighted = TRUE)
+
+# Extract graph backbone.
+bb_graph <- backbone(sem_g) # Warning: Quita los valores de la diagonal, pero no pasa nada.
+
+sem_m = matrix(0,nrow=nrow(sem_m),ncol=ncol(sem_m))
+for(i in 1:nrow(bb_graph)){
+  sem_m[bb_graph$from[i],bb_graph$to[i]] <- bb_graph$weight[i]
+}
+
+output_matrix <- file.path(opt$output, paste0("filtered_",opt$input_matrix))
+# Return the matrix network.
+npySave(output_matrix,sem_m)
