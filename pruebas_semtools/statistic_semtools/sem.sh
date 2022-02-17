d1d3e46f17436aa67427698c5cb8b03526b96bdf
Federico Garcia Criado <federogc@picasso.scbi.uma.es>
2022-02-17 17:42:28 +0100

    tests in some semtools methods

--- pruebas_semtools/statistic_semtools/sem.sh
+++ pruebas_semtools/statistic_semtools/sem.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 source ~soft_bio_267/initializes/init_ruby
-
-/mnt/home/users/bio_267_uma/federogc/software/semtools/bin/semtools.rb -d GO
-
-/mnt/home/users/bio_267_uma/federogc/software/semtools/bin/semtools.rb -i gene2go -o ./results.txt -O GO -n -S ","
+semtools.rb -i gene2go -o ./results.txt -O GO -n -S ","
