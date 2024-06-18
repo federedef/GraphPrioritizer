fa61241b17fa447b0955cec6b0ba348a4a90a775
federedef <federogc98@gmail.com>
2024-06-18 11:47:53 +0200

    adding new zampieri control update

--- scripts/aux_scripts/get_disorder_class.py
+++ scripts/aux_scripts/get_disorder_class.py
@@ -5,6 +5,18 @@ import warnings
 from py_semtools import OboParser, Ontology
 import py_exp_calc.exp_calc	as pxc
 
+def load_pairs_file(filename):
+    dict_disease_to_hp = {}
+    with open(filename, "r") as f:
+        for line in f:
+            mondo, hpo = line.strip().split("\t")
+            query = dict_disease_to_hp.get(mondo)
+            if query is None:
+                dict_disease_to_hp[mondo] = [hpo]
+            else:
+                dict_disease_to_hp[mondo].append(hpo)
+    return dict_disease_to_hp
+
 def load_file(filename):
     terms = []
     with open(filename, "r") as f:
@@ -37,71 +49,89 @@ options = vars(opts)
 # MAIN 
 ####################################
 
+
 def get_dis2dclass(diseases, disorder_class, ontology):
-	dis_class = set(disorder_class)
-	disease2disclass = {}
-	dependency_map = get_dependency_map(disorder_class)
-	term2ic = get_term2ic(dis_class, ontology)
-	for disease in diseases:
-		parents = ontology.get_ancestors(disease)
-		# intersect
-		parents = set(parents) & dis_class
-		parents = just_child_dependencies(parents, dependency_map)
-		number_classes = len(parents)
-		if number_classes == 0:
-			disclass = "unclasiffied"
-		elif number_classes > 3:
-			disclass = "multiple"
-		else:
-			max_term = ""
-			max_ic = 0
-			for parent in parents:
-				ic = term2ic[parent]
-				if ic > max_ic:
-					max_ic = ic
-					max_term = parent 
-			disclass = ontology.translate_id(max_term)	
-		disease2disclass[disease] = [disclass, number_classes]
-	return disease2disclass
+    ds_cls_terms = disorder_class.keys()
+    dis_class = set(ds_cls_terms)
+    disease2disclass = {}
+    dependency_map = get_dependency_map(ds_cls_terms)
+    term2ic = get_term2ic(dis_class, ontology)
+    for disease in diseases:
+        parents = ontology.get_ancestors(disease)
+        # intersect
+        #if "MONDO:0021147" in parents or "MONDO:0005071" in parents: continue
+        # Syndromics terms: "MONDO:0002254" Multiple
+        # direct classify: "MONDO:0045024" Cancer
+        # exclusive clasify: "MONDO:0005071" System nervious
+
+        # syndromic indicator
+        nmax = 3
+        if "MONDO:0002254" in parents: 
+            nmax = 2
+        # Direct or not classifier
+        if "MONDO:0045024" in parents: # One way terms (e.g. cancer)
+            parents = ["MONDO:0045024"]
+        else:
+            parents = set(parents) & dis_class
+            parents = just_child_dependencies(parents, dependency_map)
+        # Exlusive term
+        if "MONDO:0005071" in parents:
+                nmax = 1
+        number_classes = len(parents)
+        if number_classes == 0:
+            disclass = "unclasiffied"
+        elif number_classes > nmax:
+            disclass = "multiple"
+        else:
+            max_term = ""
+            max_ic = 0
+            for parent in parents:
+                ic = term2ic[parent]
+                if ic > max_ic:
+                    max_ic = ic
+                    max_term = parent 
+            disclass = disorder_class[max_term][0]
+            disclass = disorder_class[max_term][0]
+        disease2disclass[disease] = [disclass, number_classes]
+    return disease2disclass
 
 def just_child_dependencies(terms, dependency_map):
-	filtered_terms = terms
-	for term in terms:
-		if dependency_map.get(term):
-				terms2remove = dependency_map[term]
-				filtered_terms = pxc.diff(filtered_terms, terms2remove)
-		continue
-	return filtered_terms
-
+    filtered_terms = terms
+    for term in terms:
+        if dependency_map.get(term):
+                terms2remove = dependency_map[term]
+                filtered_terms = filtered_terms - terms2remove
+        continue
+    return filtered_terms
 
 def get_dependency_map(terms):
-	set_terms	= set(terms)
-	term2dep = {}
-	for term in terms:
-	    parentals = ontology.get_ancestors(term)
-	    dependencies = set(parentals) & set_terms
-	    if dependencies:
-	    		term2dep[term] = list(dependencies)
-	return term2dep
+    set_terms   = set(terms)
+    term2dep = {}
+    for term in terms:
+        parentals = ontology.get_ancestors(term)
+        dependencies = set(parentals) & set_terms
+        if dependencies:
+                term2dep[term] = dependencies
+    return term2dep
 
 def get_term2ic(terms, ontology):
-	term2ic = {}
-	for term in terms:
-		term2ic[term] = ontology.get_IC(term)
-	return	term2ic	
+    term2ic = {}
+    for term in terms:
+        term2ic[term] = ontology.get_IC(term)
+    return  term2ic 
 
 
 ontology = Ontology(file= options.get("ontology"), load_file = True)
-disorder_class = load_file(options["disorder_class"])
-disorder_class = [dis_class[0] for dis_class in disorder_class]
+disorder_class = load_pairs_file(options["disorder_class"])
 
-print(disorder_class)
 disease_genes = load_file(options["input_file"])
 diseases = list(set([disease for disease, genes in disease_genes]))
 disease2disclass = get_dis2dclass(diseases, disorder_class, ontology)
 
 with open(options["output_file"], "w") as f:
 	for disease, genes in disease_genes:
-		f.write(f"{disease}\t{genes}\t{disease2disclass[disease][0]}\t{disease2disclass[disease][1]}\n")
+		if disease2disclass.get(disease):
+				f.write(f"{ontology.translate_id(disease)}\t{genes}\t{disease2disclass[disease][0]}\t{disease2disclass[disease][1]}\n")
+				#f.write(f"{disease}\t{genes}\t{disease2disclass[disease][0]}\t{disease2disclass[disease][1]}\n")
 
 
--- scripts/daemon_scripts/control_preparation.sh
+++ scripts/daemon_scripts/control_preparation.sh
@@ -5,22 +5,28 @@ source ~soft_bio_267/initializes/init_ruby
   # ####################
   mkdir -p $control_genes_folder/zampieri/processed_data 
 
-  desaggregate_column_data -i ./input/input_processed/disease -x 2 | awk '{OFS="\t"}{print $2,$1}' > terms2annot
-  cut -f1 terms2annot | sort | uniq > terms_mondo
-  # Extracting OMIM cross reference
-  semtools -i terms_mondo -O ./input/input_obo/mondo.obo -o term2omim --xref_sense -k "OMIM:[0-9]*" --list
-  awk '{OFS="\t"}{sub(/OMIM:/,"",$2); print $1, $2}' term2omim > tmp && mv tmp term2omim
-  standard_name_replacer -i term2omim -I ./translators/mimTitles -c 2 > tmp && mv tmp term2omim
+  # Parse documento comorbid (disease_txt, disease_code, genes)
+  parse_morbid_omim.py $control_genes_folder/zampieri/data/omim_data/morbidmap.txt > parsed_morbid
+  # Extracting OMIM cross reference (term2omim_code)
+  cut -f2 parsed_morbid | sort | uniq > omims
+  semtools -i omims -O "MONDO" -o term2omim_code -k "OMIM:[0-9]*" --list
+  # Extracting annotation and txt assoc (term2annot, term2omim_txt)
+  standard_name_replacer -i parsed_morbid -I term2omim_code -c 2 -u > omimtxt_mondo_genes
+  cut -f 1,2 omimtxt_mondo_genes | awk '{FS="\t";OFS="\t"}{print $2,$1}' > term2omim_txt
+  cut -f 2,3 omimtxt_mondo_genes > term2annot
   # Collapse based on similarity text and common direct ancestor
-  collapse_similar_childs_into_parental.py -i terms2annot -n term2omim  -O ./input/input_obo/mondo.obo -o "$control_genes_folder/zampieri/processed_data/collapsed_annotations" -r ',/' -t "0.85" --with_annotation
-  rm terms2annot
-  rm term2omim
-  rm terms_mondo
+  collapse_similar_childs_into_parental.py -i term2annot -n term2omim_txt  -O `semtools -d list | grep "MONDO"` -o "$control_genes_folder/zampieri/processed_data/collapsed_annotations" -r ',/' -t "0.80" --with_annotation
+  #rm term2annot
+  #rm term2omim_txt
+  #rm omims
+  #rm term2omim_code
 
-  # Parent | child | gene
-  awk '{OFS="\t"}{print $1,$3}' $control_genes_folder/zampieri/processed_data/collapsed_annotations | aggregate_column_data -i - -x 1 -a 2 > disease_genes
+  # Parent | genes
+  desaggregate_column_data -i control_genes/zampieri/processed_data/collapsed_annotations -x 3 | sed "s/ //" \
+  | awk '{OFS="\t"}{if (NF==3) print $0}' | standard_name_replacer -i - -I ./translators/symbol_HGNC -c 3 -u \
+  | cut -f 2,3 | aggregate_column_data -i - -x 1 -a 2 > disease_genes
   # Parent | gene | disclass 
-  get_disorder_class.py -i disease_genes -C $control_genes_folder/zampieri/data/disorder_classes -O ./input/input_obo/mondo.obo -o $control_genes_folder/zampieri/processed_data/disease_genes_disorderclass
+  get_disorder_class.py -i disease_genes -C $control_genes_folder/zampieri/data/disorder_classes -O `semtools -d list | grep "MONDO"` -o $control_genes_folder/zampieri/processed_data/disease_genes_disorderclass
   rm disease_genes
   grep -v -E "unclasiffied|multiple" $control_genes_folder/zampieri/processed_data/disease_genes_disorderclass > tmp && mv tmp $control_genes_folder/zampieri/processed_data/disease_genes_disorderclass
 
@@ -33,7 +39,7 @@ source ~soft_bio_267/initializes/init_ruby
   # # Menche bench #
   # ################
   # mkdir -p $control_genes_folder/menche
-  # grep -v -E "RASopathy|Serpinopathy" $control_genes_folder/menche/data/menche_bench.tsv > menche_26 # removing short aggrupation from dataset
+  # grep -v -E "Rare_genetic_respiratory_disease|RASopathy|Serpinopathy" $control_genes_folder/menche/data/menche_bench.tsv > menche_26 # removing short aggrupation from dataset
   # desaggregate_column_data -i menche_26 -x 2 -s ";" > disaggregated_menche_bench.tsv
 
   # standard_name_replacer -I ./translators/symbol_HGNC -i disaggregated_menche_bench.tsv -c 2 -u | aggregate_column_data -i - -x 1 -a 2 > $control_genes_folder/menche/disease_gens
