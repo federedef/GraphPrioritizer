c00f4df8a27f184390a255e7bec7f85efb8638bd
federedef <federogc98@gmail.com>
2025-02-18 12:05:05 +0100

    remove unnecesary files from coDep data

--- testing_paper_coDep/translate.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-export PATH=~soft_bio_267/programs/x86_64/scripts:$PATH
-. ~soft_bio_267/initializes/init_ruby
-
-idconverter.rb -d ../translators/symbol_HGNC -i data_coDep -c 0,1 > translated_coDep
-
